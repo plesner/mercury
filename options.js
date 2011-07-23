a39b9dd9eb1ef90c144566c31e3a5b081a5fd8cd
Christian Plesner Hansen <plesner@ne.utrino.org>
2011-07-23 13:20:54 +1000

    Did some work on a mercury options page.

--- options.js
+++ options.js
@@ -1,5 +1,25 @@
 var mercury = new Mercury(new Chrome());
 
+// Yes, as a matter of fact I do love how inconsistent JS is.  Otherwise I
+// wouldn't have to spend my time writing junk like this.
+NodeList.prototype.toArray = function () {
+  return Array.prototype.slice.call(this);
+};
+
+function toggleVisible(element) {
+  var style = element.style;
+  if (style.display != "inline") {
+    style.display = "inline";
+  } else {
+    style.display = "none";
+  }
+}
+
+function expanderClicked(name) {
+  var spans = document.getElementsByClassName(name + "Help").toArray();
+  spans.forEach(toggleVisible);
+}
+
 function textKeyUp() {
   var entries = document.getElementById("entries");
   var text = document.getElementById("text").value;
