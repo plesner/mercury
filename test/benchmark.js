6d7204186d23a639cb06596f08f2bf1cb6d8705c
Christian Plesner Hansen <plesner@ne.utrino.org>
2011-07-27 21:00:09 +1000

    Added rudimentary search benchmark.

--- test/benchmark.js
+++ test/benchmark.js
@@ -1,4 +1,4 @@
-var kProfile = false;
+var kProfile = true;
 
 BenchmarkFakeChrome.inherit(FakeChrome);
 function BenchmarkFakeChrome() {
@@ -32,8 +32,8 @@ function addStep(text) {
   return li;
 }
 
-function recordTime(item, time) {
-  item.innerHTML += " " + time;
+function subStep(item, step) {
+  item.innerHTML += " " + step;
 }
 
 function defer(thunk) {
@@ -49,23 +49,86 @@ function deferredFor(from, to, thunk) {
   });
 }
 
-var chrome = new BenchmarkFakeChrome();
-defer(function () {
-  for (var i = 0; i < 1000; i++) {
+function measure(id, thunk) {
+  var start = new Date();
+  if (kProfile) console.profile(id);
+  thunk();
+  if (kProfile) console.profileEnd(id);
+  var end = new Date();
+  return end - start;
+}
+
+function startInstallBenchmark() {
+  var step = addStep("Running install benchmark...");
+  deferredFor(0, 10, function (i) {
+    var mercury = new Mercury(fakeChrome);
+    mercury.install();
+    fakeChrome.clearListeners();
+    var timeRecorded = fakeChrome.timeSpentBookmarking;
+    fakeChrome.timeSpentBookmarking = 0;
+    subStep(step, timeRecorded);
+  });
+}
+
+function startSearchBenchmark() {
+  var step = addStep("Running search benchmark...");
+  var mercury = new Mercury(fakeChrome);
+  mercury.install();
+  deferredFor(0, 1, function (i) {
+    var duration = measure("search", function () {
+      fakeQueries.forEach(function (query) {
+        fakeChrome.setOmniboxText(query);
+      });
+    });
+    subStep(step, duration);
+  });
+}
+
+function setupFakeBookmarks(chrome) {
+  for (var i = 0; i < 1; i++) {
     getTestBookmarks().forEach(function (bookmark) {
       chrome.addBookmark(bookmark.title, bookmark.url);
     });
   }
-});
+}
 
-function startInstallBenchmark() {
-  var step = addStep("Running install benchmark");
-  deferredFor(0, 5, function (i) {
-    var mercury = new Mercury(chrome);
-    mercury.install();
-    chrome.clearListeners();
-    var timeRecorded = chrome.timeSpentBookmarking;
-    chrome.timeSpentBookmarking = 0;
-    recordTime(step, timeRecorded);
+function isWord(str) {
+  return !!(/^\w/.exec(str));
+}
+
+function buildFakeQueries() {
+  var seen = {};
+  var all = [];
+  getTestBookmarks().forEach(function (bookmark) {
+    var title = bookmark.title;
+    var words = title.split(" ");
+    var letters = "";
+    words.forEach(function (word) {
+      if (isWord(word)) {
+        letters += word.substring(0, 1).toLowerCase();
+      }
+    });
+    for (var i = 1; i < letters.length; i++) {
+      var sub = letters.substring(0, i);
+      if (!seen[sub]) {
+        seen[sub] = true;
+        all.push(sub);
+      }
+    }
+  });
+  return all;
+}
+
+var fakeQueries;
+var fakeChrome = new BenchmarkFakeChrome();
+function setupBenchmarkData() {
+  var step = addStep("Setting up benchmark data...");
+  defer(function () {
+    setupFakeBookmarks(fakeChrome);
+    subStep(step, "bookmarks");
+    defer(function () {
+      fakeQueries = buildFakeQueries();
+      subStep(step, "queries");
+    });
   });
 }
